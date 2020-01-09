dc1273a6b3c20820ea64997959c5e1c381a86d79
Leon Malisov <leonmalisov@gmail.com>
2020-01-09 22:53:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  if array.size == 1
+    new_array = array.join
+  elsif array.size == 2
+    new_array = array.join(" and ")
+  else
+    array[array.size - 1] = "Angel"
 
+    # og_array = array
+    # array.pop
+    # new_array = array.push "and #{og_array.last}"
+    # new_array = new_array << "and #{array.last}"
+    # new_array = new_array.join(", ")
+  end
+  return new_array
 end
+
+puts oxford_comma(["kiwi", "durian", "starfruit"])
