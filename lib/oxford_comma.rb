823e00b4286de9af571e1277fd86c06ee756ac7e
Leon Malisov <leonmalisov@gmail.com>
2020-01-09 22:58:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   if array.size == 1
-    new_array = array.join
+    array.join
   elsif array.size == 2
-    new_array = array.join(" and ")
+    array.join(" and ")
   else
     array[array.size - 1] = "and #{array.last}"
-    new_array = array.join(", ")
+    array.join(", ")
   end
   return new_array
 end
